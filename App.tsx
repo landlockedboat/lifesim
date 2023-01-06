380d761b25b8e63cc1ef4dc6794fbe7f669c03aa
kobu <landlockedboat@gmail.com>
2023-01-06 19:42:27 +0100

    add typescript support

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>something happened</Text>
+      <Text>something else happened</Text>
       <StatusBar style="auto" />
     </View>
   );
